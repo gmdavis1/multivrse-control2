bfd78034067c61bddd3fbd64fe7ac128346cc377
Glenn M. Davis <gmdavis1@gmail.com>
2021-05-13 17:26:53 -0400

    remove utils module

--- index.py
+++ index.py
@@ -2,7 +2,6 @@ from bottle import template, request, Bottle, redirect
 import json
 import requests
 BASE = Bottle()
-import utils
 from websocket import create_connection
 
 CLOUD_FUNCTION_URL = "https://us-central1-mtts-307011.cloudfunctions.net/tts-synthesize"
@@ -24,33 +23,17 @@ def audiofile():
                 "speed": MECHANIC_VOICE["speed"],
                 "pitch": MECHANIC_VOICE["pitch"]}
     )
-    filename = resp.json()["file"]
-    print(f"Audio file for {t} created at {filename}")
+    filepath = resp.json()["file"]
+    print(f"Audio file for {t} created at {filepath}")
 
-    # connect to websocket and send json with the instructions/filename, then close
+    # connect to websocket and send json with the instructions/filepath, then close
     # don't need to keep the socket open here because only sending and never receiving
-    ws = create_connection("wss://rfgjune292.execute-api.us-east-2.amazonaws.com/production")
-    ws.send(filename)
+    ws = create_connection(SOCKET_URL)
+    message = {
+        "type": "audio",
+        "url": filepath
+    }
+    ws.send(json.dumps(message))
     ws.close()
-    
+
     redirect('/')
-
-# @BASE.route('/queue', method='GET')
-# def queue():
-#     msg = utils.new_message()
-#     if msg:
-#         print(msg)
-#         return msg
-#     else:
-#         return None
-
-# @BASE.route('/sock', method=['GET', 'POST'])
-# def sock_et():
-#     return template('sock.tpl')
-#     # pass parameters to the .tpl 
-
-# @BASE.route('/view-queue')
-# def vqueue():
-#     return template('poll.tpl')
-# if __name__ == '__main__':
-#     BASE.run(host='0.0.0.0', reloader=True, debug=True)
